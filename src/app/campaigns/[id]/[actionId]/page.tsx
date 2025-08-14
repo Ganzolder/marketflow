719a101b187652ec4b4961e641a6819d6425a8c3
Alexey Volobuev <gnmsapr@gmail.com>
2025-08-14 15:48:40 +0000

    Сделай эти страницы под узкие экраны

--- src/app/campaigns/[id]/[actionId]/page.tsx
+++ src/app/campaigns/[id]/[actionId]/page.tsx
@@ -264,7 +264,7 @@ export default async function ActionDetailPage({ params: paramsPromise }: Action
                                                         <TooltipProvider>
                                                             <Tooltip>
                                                                 <TooltipTrigger className="flex items-center gap-1">
-                                                                <span className="font-bold text-blue-500">{new Intl.NumberFormat(locale, currencyOptions).format(costPerUnit)}</span>
+                                                                    <span className="font-bold text-blue-500">{new Intl.NumberFormat(locale, currencyOptions).format(costPerUnit)}</span>
                                                                 </TooltipTrigger>
                                                                 <TooltipContent>
                                                                 <p>Стоимость за ед. (факт)</p>
