41c9381cd5fb3f5ae8ae6229cd883bf657e465d9
Alexey Volobuev <gnmsapr@gmail.com>
2025-09-02 04:13:52 +0000

    в кампаниях двойной скролл

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -12,7 +12,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
     <SidebarProvider>
         <div className="flex h-full">
             <AppSidebar />
-            <main className="flex-1 flex flex-col h-screen">
+            <main className="flex-1 flex flex-col">
                 <Header />
                 <div className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8">
                     {children}
